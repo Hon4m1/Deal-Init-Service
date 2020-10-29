f42fd25f82916d9efdc08a81bd13c4d8c78a66e7
Hon4m1 <silver-bird@hotmail.fr>
2020-10-29 11:33:23 +0100

    Initializing the project

---
+++ src/main/java/com/AOA/DealInitService/DealInitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.AOA.DealInitService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealInitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealInitServiceApplication.class, args);
+	}
+
+}
