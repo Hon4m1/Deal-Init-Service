1915d72f9b9ad4de799461da33388d77514750a4
Hon4m1 <silver-bird@hotmail.fr>
2020-10-30 03:35:17 +0100

    avnt confinement

--- src/main/java/com/AOA/DealInitService/rest/DealRessources.java
+++ src/main/java/com/AOA/DealInitService/rest/DealRessources.java
@@ -1,13 +1,27 @@
 package com.AOA.DealInitService.rest;
 
+import com.AOA.DealInitService.dto.DealDto;
+import com.AOA.DealInitService.model.Deal;
 import com.AOA.DealInitService.service.DealService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DealRessources {
     @Autowired
     private DealService dealService;
+
+    @PostMapping(value="/Deal")
+    public Deal create(@RequestBody Deal deal){
+        return deal;
+    }
+
+    @GetMapping(value="/Users")
+    public void sendRequest( DealDto dto){
+
+    }
+
 
 
 
--- src/main/java/com/AOA/DealInitService/service/DealService.java
+++ src/main/java/com/AOA/DealInitService/service/DealService.java
@@ -1,7 +1,13 @@
 package com.AOA.DealInitService.service;
 
+import com.AOA.DealInitService.model.Deal;
 import org.springframework.stereotype.Service;
+import springfox.documentation.spring.web.json.Json;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DealService {
+
 }
